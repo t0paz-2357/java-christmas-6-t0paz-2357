583beafd6ae77b97f4b5f26e11f4c2e4dd0f7d75
t0paz-2357 <t0paz_2357@naver.com>
2023-11-09 22:22:16 +0900

    고정으로 출력할 메세지 작성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,17 @@
 package christmas;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+		System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!");
+		System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+		System.out.println("12월 일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+		System.out.println("<주문 메뉴>");
+		System.out.println("<할인 전 총주문 금액>");
+		System.out.println("<증정 메뉴>");
+		System.out.println("<혜택 내역>");
+		System.out.println("<총혜택 금액>");
+		System.out.println("<할인 후 예상 결제 금액");
+		System.out.println("<12월 이벤트 배지>");
+	}
 }
