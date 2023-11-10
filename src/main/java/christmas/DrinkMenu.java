31b23759d830c3cb319092320acb96b3a6fc0b32
t0paz-2357 <t0paz_2357@naver.com>
2023-11-10 12:21:04 +0900

    음료 메뉴 작성

--- src/main/java/christmas/DrinkMenu.java
+++ src/main/java/christmas/DrinkMenu.java
@@ -1,19 +1,36 @@
 package christmas;
 
 public enum DrinkMenu {
+	DRINK_ZEROCOKE("제로콜라", 3000), DRINK_REDWINE("레드와인", 60000), DRINK_CHAMPAIGNE("샴페인", 25000);
 
+	private String category = "Drink";
+	private String drinkName;
+	private int drinkPrice;
+
+	DrinkMenu(String drinkName, int drinkPrice) {
+		this.drinkName = drinkName;
+		this.drinkPrice = drinkPrice;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getDrinkName() {
+		return drinkName;
+	}
+
+	public int getDrinkPrice() {
+		return drinkPrice;
+	}
 }
 
 /*
- * <애피타이저>
- * 양송이수프(6,000), 타파스(5,500), 시저샐러드(8,000)
+ * <애피타이저> 양송이수프(6,000), 타파스(5,500), 시저샐러드(8,000)
  * 
- * <메인>
- * 티본스테이크(55,000), 바비큐립(54,000), 해산물파스타(35,000), 크리스마스파스타(25,000)
+ * <메인> 티본스테이크(55,000), 바비큐립(54,000), 해산물파스타(35,000), 크리스마스파스타(25,000)
  * 
- * <디저트>
- * 초코케이크(15,000), 아이스크림(5,000)
+ * <디저트> 초코케이크(15,000), 아이스크림(5,000)
  * 
- * <음료>
- * 제로콜라(3,000), 레드와인(60,000), 샴페인(25,000)
+ * <음료> 제로콜라(3,000), 레드와인(60,000), 샴페인(25,000)
  */
