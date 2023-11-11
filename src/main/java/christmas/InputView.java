a570df265b0083ef9bd3a92dbd386ff54bd2ed56
t0paz-2357 <t0paz_2357@naver.com>
2023-11-11 13:00:32 +0900

    입력View 작성 완료

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -1,15 +1,32 @@
 package christmas;
 
+import java.util.*;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+	private static final int ORDER_MENU_NAME = 0;
+	private static final int ORDER_MENU_COUNT = 1;
+
 	public int readDate() { // 방문 날짜 입력
 		System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!");
-		return Integer.parseInt(Console.readLine());
+		int date = Integer.parseInt(Console.readLine());
+		if (date > 31 || date < 1)
+			throw new IllegalArgumentException();
+		return date;
 	}
 
-	public String readOrder() { // 메뉴와 개수 입력
+	public List<Order> readOrder() { // 메뉴와 개수 입력
 		System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-		return Console.readLine();
+		String[] inputOrder = Console.readLine().split(",");
+
+		List<Order> order = new ArrayList<Order>();
+
+		for (String input : inputOrder) {
+			String[] temp = input.split("-");
+			order.add(new Order(temp[ORDER_MENU_NAME], Integer.parseInt(temp[ORDER_MENU_COUNT])));
+		}
+
+		return order;
 	}
 }
